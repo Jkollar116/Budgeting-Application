d6abe8764676868a90992376dee45bebb332be4c
ianazimov <ianazimov@gmail.com>
2025-02-25 20:41:34 -0500

    api key line changes

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -11,7 +11,9 @@ import java.awt.*;
 import com.sun.net.httpserver.*;
 
 public class Login {
-    private static final String FIREBASE_API_KEY = "AIzaSyCMA1F8Xd4rCxGXssXIs8Da80qqP6jien8";
+
+    private static final String FIREBASE_API_KEY = System.getenv("FIREBASE_API_KEY");
+    //private static final String FIREBASE_API_KEY = "AIzaSyCMA1F8Xd4rCxGXssXIs8Da80qqP6jien8";
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
