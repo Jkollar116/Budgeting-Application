af18433be9b0cbe84fe47084486cbc0357c25228
Jay <patej13@farmingdale.edu>
2025-03-03 13:27:47 -0500

    Removing the API key to get started on fixing the login.

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -11,7 +11,7 @@ import java.awt.*;
 import com.sun.net.httpserver.*;
 
 public class Login {
-    private static final String FIREBASE_API_KEY = "AIzaSyCMA1F8Xd4rCxGXssXIs8Da80qqP6jien8";
+    private static final String FIREBASE_API_KEY = System.getenv("FIREBASE_API_KEY");
 
     public static void main(String[] args) throws Exception {
         int port = 8000;
